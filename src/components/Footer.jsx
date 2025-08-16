3966de944b51c0cb21b5d2a6dabdc2962836ede7
EMN90909 <nasongoemmanuel8@gmail.com>
2025-08-16 20:04:02 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 AlgoWhirl. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
